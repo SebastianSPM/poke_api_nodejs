d22ab04b59b98cd8a2a1db11c2734903dfffeba6
Sebsatian Pereira <sebasperemald04@gmail.com>
2026-07-07 09:13:30 -0500

    Nuevos cambios, mejorando el código - Archivos: explorar.js, pokedex.js

--- pokedex.js
+++ pokedex.js
@@ -1,11 +1,32 @@
 const prompt = require("prompt-sync")();
 const buscarPokemon = require('./explorar')
 
-const nombrepokemon = prompt("Ingresa el nombre: ").toLocaleLowerCase()
+const nombrepokemon = prompt("Ingresa el nombre: ").toLowerCase()
 
-const poke = async (nombre) => {
+const mostrarFicha = async (nombre) => {
     const pokemon = await buscarPokemon(nombre)
-    console.log("\nPOKEMON \n", "id: ", pokemon.id, "\nNombre: ", pokemon.name);
+    if(pokemon == null){
+        return;
+    }
+    console.log("\nPOKEMON\n", "id: ", pokemon.id, "\nNombre: ", (pokemon.name).toUpperCase());
+
+    console.log("\nTIPO: ");
+    
+    for (let i of pokemon.types) {
+        console.log(i.type.name);
+    }
+
+    console.log("\nESTADO: ");
+
+    for (let i of pokemon.stats) {
+        console.log(i.stat.name);
+    }
+
+    console.log("\nHabilidad: ");
+
+    for (let i of pokemon.abilities) {
+        console.log(i.ability.name);
+    }
 }
 
-poke(nombrepokemon)
+mostrarFicha(nombrepokemon)
